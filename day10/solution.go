4bdcdd774536be28b236a4aeebd40ff049b7419b
GitThePower <henrypower11@gmail.com>
2021-12-09 18:51:34 -0500

    Adding placeholders for the remaining days

---
+++ day10/solution.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// "AdventOfCode2021/helpers"
+	// "bufio"
+	// "fmt"
+	// "log"
+	// "os"
+	// "sort"
+	// "strings"
+)
+
+func main() {
+	// filename := "puzzle_input.txt"
+}
